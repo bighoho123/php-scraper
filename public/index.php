7f91f7ddd3c23ad11b394740c324d76e919015f0
Jinzhe Li <jinjinwudi@gmail.com>
2015-06-09 23:51:07 +0930

    finish the link part

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php 
+	date_default_timezone_set('UTC');
 	require("../vendor/autoload.php");
 
 	use Goutte\Client;
@@ -17,10 +18,10 @@
 <?php
 	$client = new Client();
 
-	$crwaler = $client -> request("GET","http://www.carsales.com.au/cars/results?q=(((Make%3D%5BSubaru%5D%7B%26%7DModel%3D%5BOutback%5D)%26(Year%3Drange%5B2012..%5D%26Odometer%3Drange%5B0..60000%5D))%26Service%3D%5BCarsales%5D)&sortby=~Price&cpw=1&limit=20");
+	$crawler = $client -> request("GET","http://www.carsales.com.au/cars/results?q=(((Make%3D%5BSubaru%5D%7B%26%7DModel%3D%5BOutback%5D)%26(Year%3Drange%5B2012..%5D%26Odometer%3Drange%5B0..60000%5D))%26Service%3D%5BCarsales%5D)&sortby=~Price&cpw=1&limit=20");
 
 	// Get title
-	$crwaler->filter('.result-item')->each(function ($node) {
+	$crawler->filter('.result-item')->each(function ($node) {
 		/* Title & Link */
 		$node->filter("h2 a")->each(function($node_1){
 		    print trim(preg_replace("/<span.*span>/s","",$node_1->html()))."<br>";
@@ -36,6 +37,16 @@
 		});
 	    echo "<hr>";
 	});
+
+	$next=$crawler->filter("#ctl09_p_ctl14_ctl01_headerPagination_hlNextLink");
+	$i=0;
+	while ($i<=2) {
+		$link=$next->link();
+		echo $link->getUri()."<br>";
+		$crawler=$client->click($link);
+		$next=$crawler->filter("#ctl09_p_ctl14_ctl01_headerPagination_hlNextLink");
+		$i++;
+	}
 ?>
 	</p>
 </body>
